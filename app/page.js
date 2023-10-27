6a9f0390f1ecc11cec3e4a1be395468abdea4b1a
BeckFW <beck@elbo.me>
2023-10-27 22:08:41 +0100

    Initial GestureRecogniser setup - using local model - Based on [example](https://codepen.io/mediapipe-preview/pen/zYamdVd) - Refactor for Nextjs

--- app/page.js
+++ app/page.js
@@ -1,113 +1,185 @@
-import Image from 'next/image'
+"use client"
+import { FilesetResolver, GestureRecognizer, DrawingUtils } from "@mediapipe/tasks-vision";
+import { useEffect, useState } from "react"; 
 
-export default function Home() {
+
+export default function Home () {
+
+  const videoHeight = "360px";
+  const videoWidth = "480px";
+
+  const UMConstraints = {
+    video: true,
+  }
+
+  let lastVideoTime = -1;
+  let results = undefined; 
+  let webcamRunning = false;
+  let video, canvasElement, canvasCtx, gestureOutput, playerMoveDisplay, playerMove; 
+
+  let gestureRecognizer; 
+
+  const setupVision = async () => {
+    // Create task for image file processing:
+    const vision = await FilesetResolver.forVisionTasks(
+      // path/to/wasm/root
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
+    );
+
+    return vision; 
+  }
+
+  const setupGestureRec = async (vision) => {
+    const gestureRecognizer = await GestureRecognizer.createFromOptions(vision, {
+      baseOptions: {
+        modelAssetPath: "/models/gesture_recognizer.task"
+      },
+      numHands: 2,
+      runningMode: "video", 
+    });
+
+    return gestureRecognizer;
+  }
+
+  useEffect(()=>{
+    // Once the page has loaded // 
+    async function assignTasks() {
+      const vision = await setupVision(); 
+      gestureRecognizer = await setupGestureRec(vision); 
+    }
+    // Setup GestureRecognition Tasks
+    assignTasks();
+    // Access document elements
+    getDocumentElements();
+  }, [])
+
+  // Access document elements // 
+  const getDocumentElements = () => {
+    try {
+    video = document.getElementById("webcam");
+    canvasElement = document.getElementById("output_canvas");
+    canvasCtx = canvasElement.getContext("2d");
+    gestureOutput = document.getElementById("gesture_output");
+    playerMoveDisplay = document.querySelector("#playerMoveDisplay");
+    console.info("Document elements found"); 
+    } catch (error) {
+      console.warn('Error accessing document elements'); 
+      console.log(error); 
+    }
+  }
+
+  const enableCam = (e) => { 
+    e.preventDefault();
+    if (!webcamRunning) {
+      webcamRunning = true; 
+      // Activate the webcam stream.
+      navigator.mediaDevices.getUserMedia(UMConstraints).then(function (stream) {
+        video.srcObject = stream;
+        video.addEventListener("loadeddata", predictWebcam);
+      });
+    } else { 
+      let tracks = video.srcObject.getTracks(); 
+      tracks.forEach(track => {
+        track.stop();
+      });
+      video.srcObject = null; 
+      webcamRunning = false; 
+    }
+
+  }
+
+
+  const predictWebcam = async () => {
+    const webcamElement = document.getElementById("webcam");
+    // Now let's start detecting the stream.
+
+    let nowInMs = Date.now();
+    if (video.currentTime !== lastVideoTime) {
+      lastVideoTime = video.currentTime;
+      results = gestureRecognizer.recognizeForVideo(video, nowInMs);
+    }
+
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    const drawingUtils = new DrawingUtils(canvasCtx);
+
+    canvasElement.style.height = videoHeight;
+    webcamElement.style.height = videoHeight;
+    canvasElement.style.width = videoWidth;
+    webcamElement.style.width = videoWidth;
+
+    if (results.landmarks) {
+      for (const landmarks of results.landmarks) {
+        drawingUtils.drawConnectors(
+          landmarks,
+          GestureRecognizer.HAND_CONNECTIONS,
+          {
+            color: "#00FF00",
+            lineWidth: 5
+          }
+        );
+        drawingUtils.drawLandmarks(landmarks, {
+          color: "#FF0000",
+          lineWidth: 2
+        });
+      }
+    }
+    canvasCtx.restore();
+    if (results.gestures.length > 0) {
+      gestureOutput.style.display = "block";
+      playerMoveDisplay.style.display = "block";
+      gestureOutput.style.width = videoWidth;
+      const categoryName = results.gestures[0][0].categoryName;
+      const categoryScore = parseFloat(
+        results.gestures[0][0].score * 100
+      ).toFixed(2);
+      const handedness = results.handednesses[0][0].displayName;
+
+      switch (categoryName) {
+        case "Victory":
+          playerMove = "Scissors";
+          break; 
+        case "Closed_Fist":
+          playerMove = "Rock"; 
+          break;
+        case "Open_Palm":
+          playerMove = "Paper"; 
+          break;
+        default:
+          playerMove = "None";
+          break;
+      }
+
+      gestureOutput.innerText = `Confidence: ${categoryScore}%`;
+      playerMoveDisplay.innerText = `You Played ${playerMove}!`; 
+    } else {
+      gestureOutput.style.display = "none";
+      playerMoveDisplay.style.display = "none";
+    }
+    // Call this function again to keep predicting when the browser is ready.
+    if (webcamRunning === true) {
+      window.requestAnimationFrame(predictWebcam);
+    }
+}
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div>
+      <div className='text-center items-center p-10'>
+        Gesture Control System
+      </div>
+
+      <div id="liveView" className="videoView">
+        <button id="webcamButton" className="mdc-button mdc-button--raised">
+          <span className="mdc-button__ripple"></span>
+          <span className="mdc-button__label" onClick={enableCam}>ENABLE WEBCAM</span>
+        </button>
+        <div className='relative'>
+          <video id="webcam" autoPlay playsInline></video>
+          <canvas className="output_canvas absolute left-0 top-0" id="output_canvas" width="1280" height="720"></canvas>
+          <p className="text-2xl" id="playerMoveDisplay"></p>
+          <p id='gesture_output' className="output"></p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
