e335832750d09e02200bb6d4f2e7a1f20e887c79
BeckFW <beck@elbo.me>
2024-03-21 11:04:03 +0000

    Update menuButton styling for dark mode

--- components/menu-button.js
+++ components/menu-button.js
@@ -1,7 +1,7 @@
 export default function MenuButton({children, action}) {
     return(
         <div>
-            <button onClick={action} className="p-2 font-semibold text-xl bg-cyan-500 rounded w-full px-20 dark:hover:bg-cyan-800 hover:bg-cyan-300 hover:text-white drop-shadow-sm">{children}</button>
+            <button onClick={action} className="p-2 font-semibold text-xl bg-cyan-500 rounded w-full px-20 dark:hover:bg-cyan-800 dark:hover:text-cyan-300 hover:bg-cyan-300 hover:text-cyan-800 text-white drop-shadow-sm">{children}</button>
         </div>
     )
 }
