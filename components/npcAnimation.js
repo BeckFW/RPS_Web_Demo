f0f80e0d062758b93a6ac7beda4a78682b522bf0
BeckFW <beck@elbo.me>
2024-04-23 13:37:04 +0100

    Add component to display and animate NPC move

---
+++ components/npcAnimation.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+
+export default function NpcAnimation ({currentNpcMove, animate}) {
+    const [isPulsing, setIsPulsing] = useState(false); // should the animation play
+    const [npcMoveSymbol, setNpcMoveSymbol] = useState({"rock": "👊", "paper": "✋", "scissors": "✌️"}); // possible animation content
+
+    // Trigger pulse animation when animate changes
+    useEffect(() => {
+      if (animate) {
+        handlePulse();
+      }
+    }, [animate]);
+
+    const handlePulse = () => {
+        // Trigger pulse animation
+        setIsPulsing(true);
+
+        // Reset the pulse after a short delay (adjust as needed)
+        setTimeout(() => {
+        setIsPulsing(false);
+        }, 500);
+    };
+
+    return (
+        <motion.div
+        className="text-8xl text-center"
+        animate={isPulsing ? { scale: 1.4 } : { scale: 1 }}
+        transition={{ duration: 0.4, ease: "easeInOut"}}
+        >
+        {npcMoveSymbol[currentNpcMove] || npcMoveSymbol["rock"]}
+        </motion.div>
+    );
+};
