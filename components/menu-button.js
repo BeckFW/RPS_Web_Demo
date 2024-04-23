93e8d6daec92a1e2f133af75ab7d2b7bc2ff27b0
BeckFW <beck@elbo.me>
2024-04-23 18:09:35 +0100

    Add alternate style to button

--- components/menu-button.js
+++ components/menu-button.js
@@ -1,7 +1,7 @@
-export default function MenuButton({children, action}) {
+export default function MenuButton({children, action, danger}) {
     return(
         <div>
-            <button onClick={action} className="p-2 font-semibold text-xl bg-cyan-500 rounded w-full px-20 dark:hover:bg-cyan-800 dark:hover:text-cyan-300 hover:bg-cyan-300 hover:text-cyan-800 text-white drop-shadow-sm">{children}</button>
+            <button onClick={action} className={`${danger ? "bg-cyan-700 hover:bg-cyan-600 hover:text-white" : ""} py-3 font-semibold text-xl bg-cyan-500 rounded w-full px-10 dark:hover:bg-cyan-800 dark:hover:text-cyan-300 hover:bg-cyan-300 hover:text-cyan-800 text-white drop-shadow-sm`}>{children}</button>
         </div>
     )
 }
