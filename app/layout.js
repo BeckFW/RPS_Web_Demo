193da98e9af3f4e6e889b26c4cd14a44a4c0e9b1
BeckFW <beck@elbo.me>
2024-04-27 15:07:23 +0100

    Update Toaster so more toasts are shown

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <Toaster richColors />
+        <Toaster richColors expand visibleToasts={5} />
         </body>
     </html>
   )
